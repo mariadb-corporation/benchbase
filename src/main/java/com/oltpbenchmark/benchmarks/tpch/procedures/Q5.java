ca53672e2c5e2fa1c439033beb683101169d22d1
Orlando Moreno <orlando.moreno@mariadb.com>
2023-01-13 10:21:51 -0700

    PTC workaround

--- src/main/java/com/oltpbenchmark/benchmarks/tpch/procedures/Q5.java
+++ src/main/java/com/oltpbenchmark/benchmarks/tpch/procedures/Q5.java
@@ -45,7 +45,7 @@ public class Q5 extends GenericQuery {
                AND l_orderkey = o_orderkey
                AND l_suppkey = s_suppkey
                AND c_nationkey = s_nationkey
-               AND s_nationkey = n_nationkey
+               AND c_nationkey = n_nationkey
                AND n_regionkey = r_regionkey
                AND r_name = ?
                AND o_orderdate >= DATE(?)
