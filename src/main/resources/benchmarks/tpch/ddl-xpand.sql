35c7a8d72eaf229194ad4cff09ff04f0cce4f4c4
Orlando Moreno <orlando.moreno@mariadb.com>
2023-02-03 16:41:46 -0700

    remove duplicate and lineitem indexes

--- src/main/resources/benchmarks/tpch/ddl-xpand.sql
+++ src/main/resources/benchmarks/tpch/ddl-xpand.sql
@@ -25,7 +25,6 @@ CREATE TABLE nation (
     CONSTRAINT nation_ibfk_1 FOREIGN KEY (n_regionkey) REFERENCES region (r_regionkey) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) REPLICAS = ALLNODES CHARACTER SET utf8 
 ;
-CREATE INDEX nation_fk1 on nation (n_regionkey) DISTRIBUTE=1;
 CREATE INDEX idx_name on nation (n_name) DISTRIBUTE=1;
 
 CREATE TABLE part (
@@ -59,7 +58,6 @@ CREATE TABLE supplier (
     CONSTRAINT supplier_ibfk_1 FOREIGN KEY (s_nationkey) REFERENCES nation (n_nationkey) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) CHARACTER SET utf8
 ;
-CREATE INDEX supplier_fk1 on supplier (s_nationkey) DISTRIBUTE=1;
 CREATE INDEX idx_comment on supplier (s_comment) DISTRIBUTE=1;
 
 CREATE TABLE partsupp (
@@ -73,7 +71,6 @@ CREATE TABLE partsupp (
     CONSTRAINT partsupp_ibfk_2 FOREIGN KEY (ps_partkey) REFERENCES part (p_partkey) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) CHARACTER SET utf8
 ;
-CREATE INDEX partsupp_fk1 on partsupp (ps_suppkey) DISTRIBUTE=1;
 CREATE INDEX idx_supplycost on partsupp (ps_supplycost) DISTRIBUTE=1;
 
 CREATE TABLE customer (
@@ -91,7 +88,6 @@ CREATE TABLE customer (
 ;
 CREATE INDEX idx_mktsegment on customer (c_mktsegment) DISTRIBUTE=1;
 CREATE INDEX idx_acctbal on customer (c_acctbal) DISTRIBUTE=1;
-CREATE INDEX customer_fk1 on customer (c_nationkey) DISTRIBUTE=1;
 
 CREATE TABLE orders (
     o_orderkey      integer        NOT NULL,
@@ -107,7 +103,6 @@ CREATE TABLE orders (
     CONSTRAINT orders_ibfk_1 FOREIGN KEY (o_custkey) REFERENCES customer (c_custkey) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) CHARACTER SET utf8
 ;
-CREATE INDEX orders_fk1 on orders (o_custkey) DISTRIBUTE=1;
 CREATE INDEX idx_orderstatus on orders (o_orderstatus) DISTRIBUTE=1;
 CREATE INDEX idx_orderdate on orders (o_orderdate) DISTRIBUTE=1;
 
@@ -133,11 +128,3 @@ CREATE TABLE lineitem (
     CONSTRAINT lineitem_ibfk_1 FOREIGN KEY (l_orderkey) REFERENCES orders (o_orderkey) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) CHARACTER SET utf8
 ;
-CREATE INDEX lineitem_fk2 on lineitem (l_partkey, l_suppkey) DISTRIBUTE=2;
-CREATE INDEX idx_shipmode on lineitem (l_shipmode) DISTRIBUTE=1;
-CREATE INDEX idx_shipinstruct on lineitem (l_shipinstruct) DISTRIBUTE=1;
-CREATE INDEX idx_shipdate on lineitem (l_shipdate) DISTRIBUTE=1;
-CREATE INDEX idx_returnflag on lineitem (l_returnflag) DISTRIBUTE=1;
-CREATE INDEX idx_receiptdate on lineitem (l_receiptdate) DISTRIBUTE=1;
-CREATE INDEX idx_quantity on lineitem (l_quantity) DISTRIBUTE=1;
-CREATE INDEX idx_discount on lineitem (l_discount) DISTRIBUTE=1;
